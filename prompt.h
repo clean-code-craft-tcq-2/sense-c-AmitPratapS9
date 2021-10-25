2f9fde3d747afb77376a9b5caa38427785390101
Amit Pratap Singh <47778552+AmitPratapS9@users.noreply.github.com>
2021-10-25 23:08:21 +0530

    Update prompt.h

--- prompt.h
+++ prompt.h
@@ -2,6 +2,6 @@
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
-                                                                                         */
+                                                                                         
 void emailAlerter(void);
 void ledAlerter(void);
