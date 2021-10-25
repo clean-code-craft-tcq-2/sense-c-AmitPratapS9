6c682e23606a43fa65fbd7e966c9f3bd45d4251f
Amit Pratap Singh <47778552+AmitPratapS9@users.noreply.github.com>
2021-10-25 22:56:46 +0530

    Create prompt.h

---
+++ prompt.h
@@ -0,0 +1,7 @@
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
+                                                                                         */
+void emailAlerter(void);
+void ledAlerter(void);
