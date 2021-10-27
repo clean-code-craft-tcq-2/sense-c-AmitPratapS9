3ff94cc1b83d74a7436ff5436e5bbd84f5e6a2db
Amit Pratap Singh <47778552+AmitPratapS9@users.noreply.github.com>
2021-10-27 22:40:19 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -10,21 +10,19 @@
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    struct Stats_list computedStats = compute_statistics_list(numberset, setlength);
     float epsilon = 0.001;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average is NaN for empty array") {
-    //Stats computedStats = compute_statistics(0, 0);
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
+TEST_CASE("NaN for empty array list") {
+    
      float numberset[] = {0.0};
     int setlength = 0;
     bool averge;
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    struct Stats_list computedStats = compute_statistics_list(numberset, setlength);
     
      
     averge = isnan(computedStats.average)?true:false;
@@ -41,7 +39,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    struct Stats computedStats = compute_statistics_list(numberset, setlength);
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
